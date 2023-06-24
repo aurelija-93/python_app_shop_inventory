130a86b8e29df4ea7cf19db204956ffc68b13af4
Aurelija Bellerby <aurelija.bellerby@gmail.com>
2023-06-24 11:48:39 +0100

    added sql

---
+++ db/shop_inventory.sql
@@ -0,0 +1,20 @@
+DROP TABLE IF EXISTS products;
+DROP TABLE IF EXISTS suppliers;
+
+CREATE TABLE suppliers (
+    id SERIAL PRIMARY KEY,
+    name VARCHAR(255),
+    phone VARCHAR(255),
+    email VARCHAR(255)
+);
+
+CREATE TABLE products (
+    id SERIAL PRIMARY KEY,
+    name VARCHAR(255),
+    description TEXT,
+    stock INT,
+    purchase_price INT,
+    selling_price INT,
+    margin INT,
+    supplier_id INT REFERENCES suppliers(id) ON DELETE CASCADE
+);
